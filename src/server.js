113a682f96a10048e2d01218baa288df5deaab6e
3Dvaka <108063763+3Dvaka@users.noreply.github.com>
2025-02-02 21:25:51 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
     cors: {
-        origin: "http://localhost:3000",  
+        origin: "http://localhost:3000",  'https://testchat-ma1c.vercel.app',
         methods: ["GET", "POST"]
     }
 });
