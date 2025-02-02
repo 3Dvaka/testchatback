52010e9a02863826a2c593a69b5b22376e7fc9b8
3Dvaka <108063763+3Dvaka@users.noreply.github.com>
2025-02-02 21:18:21 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use('/api/messages', messageRoutes);
 
 
 const corsOptions = {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://testchat-ma1c.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, 
 };
