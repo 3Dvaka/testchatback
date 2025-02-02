bec5240e858271cc8e12e8d50d1eda40e5d088cc
3Dvaka <108063763+3Dvaka@users.noreply.github.com>
2025-02-02 23:25:53 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,19 +7,19 @@ const messageRoutes = require('./routes/messageRoutes');
 const cors = require('cors');
 
 app.use(express.json());
-app.use(cors(corsOptions));
-
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/chats', chatRoutes);
-app.use('/api/messages', messageRoutes);
-
 
 const corsOptions = {
     origin: ['http://localhost:3000', 'https://testchat-ma1c.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, 
 };
+
+app.use(cors(corsOptions));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/chats', chatRoutes);
+app.use('/api/messages', messageRoutes);
 
 
 module.exports = app;
