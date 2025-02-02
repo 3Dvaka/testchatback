e332d4ef66e0d2d9cb81636d12c5af6bfb0bc3bb
3Dvaka <108063763+3Dvaka@users.noreply.github.com>
2025-02-02 20:48:34 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const socketIo = require('socket.io');
 
 const PORT = 5000;
-const MONGO_URI = 'mongodb+srv://vladleurda02:ree1IndvHO3ZPgOs@main.n0hck.mongodb.net/?retryWrites=true&w=majority&appName=main';
+const MONGO_URI = 'mongodb://mongo:CcOcHDqAUpxkGAxGbdMfoVIRNTwHWYQd@mongodb.railway.internal:27017';
 
 const connectDB = async () => {
     try {
